ab31e6b5746ef41c8f9e781a1f86342d21ceccf7
Narayan Singh <rjsameer60@gmail.com>
2025-05-30 19:18:06 +0530

    starting middleware

--- src/routers/ping.router.ts
+++ src/routers/ping.router.ts
@@ -1,6 +1,9 @@
-import { Express } from 'express';
+import express from 'express';
 import { pingHanlder } from '../controllers/ping.controller';
 
-export function createPingRouter(app: Express){
-    app.get('/ping', pingHanlder);
-}
+
+const pingRouter = express.Router();
+
+pingRouter.get('/ping', pingHanlder);
+
+export default pingRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { ServerConfig } from "./config";
-import { createPingRouter } from "./routers/ping.router";
+import pingRouter from "./routers/ping.router";
 
 const app = express();
 
-createPingRouter(app);
+/* 
+    * Registering the ping router and their corresponding routes with out app server object.
+*/
+app.use(pingRouter);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Server running on port ${ServerConfig.PORT}`);
